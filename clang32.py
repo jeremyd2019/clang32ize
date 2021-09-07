2b7f52194b9d07dff8785ebd707ea64a8d996f9a
Jeremy Drake <github@jdrake.com>
2021-09-06 21:27:26 -0700

    add verbose option to output tree of blocked dependencies

--- clang32.py
+++ clang32.py
@@ -25,10 +25,21 @@ PKGBASE_BLACKLIST = frozenset(itertools.chain((
 
 del x
 
+def dep_tree(p, forward, reverse, level=0, seen=None):
+    if seen is None:
+        seen = set()
+    print(f"{'│   ' * level}├── {p}{ f': blocked by {forward[p]}' if level == 0 and p in forward else ''}")
+    seen.add(p)
+    for p2 in reverse.get(p, []):
+        if p2 not in seen:
+            dep_tree(p2, forward, reverse, level+1, seen)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Mark PKGBUILDs for clang32')
     parser.add_argument('--allclang64', action='store_true')
     parser.add_argument('--depth', type=int, default=1)
+    parser.add_argument('--verbose', action='store_true')
     args = parser.parse_args()
 
     r = pacdb.mingw_db_by_name('clang64')
@@ -44,6 +55,8 @@ if __name__ == "__main__":
 
         bases = set()
         mingwpkg = re.compile(r"^mingw-w64-clang-")
+        forward = {}
+        reverse = {}
         for _ in range(args.depth):
             newprovs = set()
             for p in r:
@@ -54,6 +67,13 @@ if __name__ == "__main__":
                     if mingwpkg.match(d) and d.replace('-x86_64-', '-i686-') not in sprovs:
                         alldeps = False
                         break
+                if args.verbose and not alldeps:
+                    sname = p.name.replace('-x86_64-', '-i686-')
+                    blockers = set(d.replace('-x86_64-', '-i686-') for d in deps if mingwpkg.match(d)) - sprovs
+                    forward[sname] = blockers
+                    for b in blockers:
+                        reverse.setdefault(b, set()).add(sname)
+
                 if alldeps and p.base not in PKGBASE_BLACKLIST and \
                         p.name.replace('-x86_64-', '-i686-') not in s.byname:
                     bases.add(p.base)
@@ -61,6 +81,12 @@ if __name__ == "__main__":
                     for prov in p.provides:
                         newprovs.add(prov.replace('-x86_64-', '-i686-'))
             sprovs.update(newprovs)
+            if args.verbose:
+                print("Blocked dependency tree for clang32:")
+                for p in reverse:
+                    if p not in forward or forward[p] == set((p,)):
+                        dep_tree(p, forward, reverse)
+
     else:
         bases = set(p.base for p in r if p.base not in PKGBASE_BLACKLIST)
 
